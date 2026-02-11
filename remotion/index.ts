9c85dba0d68f0374de58e4ed37c9f3755d6be903
lizarlfos1 <blizzardoctopus@gmail.com>
2026-02-11 19:39:46 +1100

    Fix Remotion import path for Subtitles

--- remotion/index.ts
+++ remotion/index.ts
@@ -1,4 +1,4 @@
 import { registerRoot } from "remotion";
-import { SubtitlesComposition } from "./Subtitles.js";
+import { SubtitlesComposition } from "./Subtitles";
 
 registerRoot(SubtitlesComposition);
